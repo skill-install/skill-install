c4dacfb0401511f896cee6f132ed34e40bcd8bd5
秦頌光 <first1freedom@gmail.com>
2016-11-29 17:48:35 +0900

    m2[update]一覧のhtml作成

--- skill-install/resources/views/index.blade.php
+++ skill-install/resources/views/index.blade.php
@@ -9,5 +9,29 @@
 @endsection
 
 @section('body')
-
+<div id="wrapper">
+    <div id="free_video">
+        <div class="video_wrapper">
+            <div class="thumbnail">
+                <img class="video_img" src="">
+                <div>
+                    <img class="creator_img" src="">
+                    <div class="video_title"></div>
+                    <div class="creator_name"></div>
+                </div>
+            </div>
+            <div class="sub_info">
+                <div class="video_view"></div>
+                <div class="membership"></div>
+                <div class="category"></div>
+            </div>
+        </div>
+    </div>
+    <div id="new_video">
+        
+    </div>
+    <div id="repeat_video">
+        
+    </div>
+</div>
 @endsection
