e38e4b89c5cc54b5a2f9134ca78b272b143f8157
USER <user@USER-no-MacBook-Air.local>
2016-11-29 21:35:15 +0900

    #6[update]ProfileモデルとPrivateProfileモデルの初期設定

--- skill-install/app/PrivateProfile.php
+++ skill-install/app/PrivateProfile.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class PrivateProfile extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nickname','img'
+    ];
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
 }
--- skill-install/app/Profile.php
+++ skill-install/app/Profile.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nickname','img'
+    ];
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+    
 }
