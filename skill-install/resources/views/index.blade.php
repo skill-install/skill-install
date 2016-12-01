d4bc8c57ec53ff2ddaa58c77d427f0b730322a18
秦頌光 <first1freedom@gmail.com>
2016-12-02 00:27:39 +0900

    #15[update]コミュニティー一覧html/css

--- skill-install/resources/views/index.blade.php
+++ skill-install/resources/views/index.blade.php
@@ -42,19 +42,33 @@
     <!-- コミュニティー一覧 -->
     <div class="list_title">新着コミュニティー</div>
     <div id="new_comunity">
-        @for($i = 0; $i < 3; $i++)
+        @for($i = 0; $i < 6; $i++)
             <div class="comunity">
-                <img class="comunity_img" src="/img/test.jpg">
-                <div class="cover">
-                    <div class="comunity_info">
-                        <div class="follow"><i class="fa fa-users" aria-hidden="true"></i> 729</div>
-                        <div class="info_bottom">
-                            <div class="comunity_title">秦の楽しいサッカー</div>
-                            <div class="category_wrapper">
-                                <div class="category">サッカー</div>
-                                <div class="category">ミッドフィルダー</div>
-                                <div class="category">トップ下</div>
+                <div class="comunity_front">
+                    <img class="comunity_img" src="/img/test.jpg">
+                    <div class="cover">
+                        <div class="comunity_info">
+                            <div class="follow"><i class="fa fa-users" aria-hidden="true"></i> 729</div>
+                            <div class="info_bottom">
+                                <div class="comunity_title">秦の楽しいサッカー</div>
+                                <div class="category_wrapper">
+                                    <div class="category">サッカー</div>
+                                    <div class="category">ミッドフィルダー</div>
+                                    <div class="category">トップ下</div>
+                                </div>
                             </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="comunity_back">
+                    <img class="comunity_img" src="/img/test.jpg">
+                    <div class="cover">
+                        <div class="center">
+                            <span>
+                                <div class="comunity_lesson">マークの外し方</div>
+                                <div class="comunity_lesson">フリーキックの蹴り方</div>
+                                <div class="comunity_lesson">裏への抜け方</div>
+                            </span>
                         </div>
                     </div>
                 </div>
